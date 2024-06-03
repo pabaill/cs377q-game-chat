8864d0a63fe7ff0c753edd807cd51995c225c5e3
Ellie Yang <elliezy@stanford.edu>
2024-06-02 19:56:31 -0700

    merge

--- scripts/content.js
+++ scripts/content.js
@@ -300,8 +300,6 @@ function createDraggableBox() {
   document.body.appendChild(container);
 }
 
-<<<<<<< Updated upstream
-=======
 function getArrayDisplayInnerHTML(arr) {
   const reversedArray = arr.slice().reverse();
   let innerHTMLContent = '';
@@ -317,7 +315,6 @@ function getArrayDisplayInnerHTML(arr) {
   return innerHTMLContent;
 }
 
->>>>>>> Stashed changes
 async function gptUpdateChatWindow(messages, content, chatButton, gameButton) {
   const gameResources = getGameElements();
   content.innerHTML = "loading...";
@@ -415,12 +412,8 @@ async function gptUpdateGameWindow(gamelog, content, chatButton, gameButton) {
       let res = data.choices[0].message.content;
       numGameActionsSeen = gamelog.length;
       res = replaceString(res, gameResources);
-<<<<<<< Updated upstream
-      content.innerHTML = res;
-=======
       previousGameSummaries.push(res);
       content.innerHTML = getArrayDisplayInnerHTML(previousGameSummaries);
->>>>>>> Stashed changes
       content.appendChild(chatButton);
       content.appendChild(gameButton);
     })
